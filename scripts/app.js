0307e129ac3d0a078e3a3b1808303550e062e0ef
John Benson <johnbenson@gmail.com>
2016-09-06 12:40:39 -0400

    recently played now pulls from Last.fm API

--- scripts/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('lab17Module', []);
-
-app.config(function($scope){
-  // do stuff with the $scope
-});
---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+var config = {
+  API_KEY : "6990d4d73a7df411ec925a321eee73ce"
+}
